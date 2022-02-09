c46159aff72904c06af752bf3d32d60a384b60e1
wesley <wesley.yang789@hotmail.com>
2022-02-09 14:38:08 +0800

    modify data-type.js

--- data-types.js
+++ data-types.js
@@ -1,2 +1,25 @@
 // data-types.js
 // 数据类型
+
+// JavaScript的数据类型
+//
+// 数值(number): 整数和小数(比如1、3.14)
+// 字符串(string): 文本(比如Hello World)
+// 布尔值(boolean): true, false
+//
+// * 以上三种类型为原始类型(primitive type)，最基础的数据类型
+// 不可再细分
+
+// undefined: 表示“未定义”
+// null: 表示空值
+//
+// * undefined和null可以看成两个特殊值
+
+// 对象(object): 各种值组成的集合
+// 对象称为合成类型(complex type)的值，一个对象是多个原始类型
+// 的值合成的，可以把把对象看成一个存放这种值的容器
+//
+// 对象是复杂得数据类型，又可以把对象分为三个子类型，如下
+// * 狭义的对象: object
+// * 数组: array
+// * 函数: function 
